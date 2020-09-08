338d9ddaf99518cd2776c3513fd860eaa8c30a8a
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-09-08 13:37:56 -0400

    Fix LinkedList imports and remove duplicate files

--- stack/stack.py
+++ stack/stack.py
@@ -10,7 +10,9 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
-from linked_list import LinkedList
+import sys
+sys.path.append("../singly_linked_list/")
+from singly_linked_list import LinkedList
 
 # First implementation of Stack class (using just an array)
 # class Stack:
