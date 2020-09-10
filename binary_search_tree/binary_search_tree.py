0274bb3ac593ba81ccca17a12da52d7d16fb3a3b
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-09-10 18:24:40 -0400

    BST traversal methods

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -82,17 +82,54 @@ class BSTNode:
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self):
-        pass
+
+        if self.left:
+            self.left.in_order_print()
+        print(self.value)
+
+        if self.right:
+            self.right.in_order_print()
+
+
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
+
     def bft_print(self):
-        pass
+        # Using a queue
+        queue = []
+
+        # Add the root node to the queue
+        queue.append(self)
+
+        while len(queue) > 0:
+            # Queue (FIFO) so we need to pop index 0!
+            current_node = queue.pop(0)
+            print(current_node.value)
+
+            if current_node.left:
+                queue.append(current_node.left)
+            if current_node.right:
+                queue.append(current_node.right)
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
+
     def dft_print(self):
-        pass
+        # Using a stack
+        stack = []
+
+        # Add the root node to the stack
+        stack.append(self)
+
+        while len(stack) > 0:
+            current_node = stack.pop()
+            print(current_node.value)
+            if current_node.right:
+                stack.append(current_node.right)
+            if current_node.left:
+                stack.append(current_node.left)
+
 
     # Stretch Goals -------------------------
     # Note: Research may be required
